75d9addeca2288f46b78e74720208425f993e791
dbc2201 <divyanshb22@gmail.com>
2020-10-22 15:09:41 +0530

    create MainThreadDemo.java

---
+++ src/sectionI/thread/main/MainThreadDemo.java
@@ -0,0 +1,11 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 22/10/20
+ *  Time: 3:09 PM
+ *  File Name : MainThreadDemo.java
+ * */
+package sectionI.thread.main;
+
+public class MainThreadDemo {
+
+}
