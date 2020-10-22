4024184e3773a1976d4c5825acdea890787eb27c
dbc2201 <divyanshb22@gmail.com>
2020-10-22 15:21:57 +0530

    add an example to sleep for some time

--- src/sectionI/thread/examples/Example1.java
+++ src/sectionI/thread/examples/Example1.java
@@ -7,7 +7,15 @@
 package sectionI.thread.examples;
 
 public class Example1 {
+    private static final int LIMIT = 10;
     public static void main(String[] args) {
-
+        for (int index = 0; index < LIMIT; index++) {
+            System.out.println(index);
+            try {
+                Thread.sleep(1000L); // wait for 300 ms
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
