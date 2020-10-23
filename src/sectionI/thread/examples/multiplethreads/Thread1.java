5016d123cfce0f20ab632e14c5c37e6dd8dc7212
dbc2201 <divyanshb22@gmail.com>
2020-10-23 16:12:45 +0530

    create Thread1.java

---
+++ src/sectionI/thread/examples/multiplethreads/Thread1.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 23/10/20
+ *  Time: 4:12 PM
+ *  File Name : Thread1.java
+ * */
+package sectionI.thread.examples.multiplethreads;
+
+public class Thread1 {
+}
