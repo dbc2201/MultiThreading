514f785ba878d58560e3cca6abffe6062af34b5f
dbc2201 <divyanshb22@gmail.com>
2020-10-23 16:17:43 +0530

    add a main method in an execution class

---
+++ src/sectionI/thread/examples/multiplethreads/Main.java
@@ -0,0 +1,13 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 23/10/20
+ *  Time: 4:17 PM
+ *  File Name : Main.java
+ * */
+package sectionI.thread.examples.multiplethreads;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
