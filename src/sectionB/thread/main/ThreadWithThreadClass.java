37c3059a4805e336237b35a948dc8da4cd55658a
dbc2201 <divyanshb22@gmail.com>
2020-10-23 12:40:23 +0530

    create a thread with the Thread class

--- src/sectionB/thread/main/ThreadWithThreadClass.java
+++ src/sectionB/thread/main/ThreadWithThreadClass.java
@@ -8,4 +8,21 @@ package sectionB.thread.main;
 
 public class ThreadWithThreadClass extends Thread {
 
+    private static final int LIMIT = 10;
+
+    // 1. create a public constructor
+    public ThreadWithThreadClass() {
+        // call the super constructor
+        super("T2");
+    }
+
+    // 2. override the run method from the Thread class
+    @Override
+    public void run() {
+        System.out.println("T2 has started.");
+        for (int index = 0; index < LIMIT; index++) {
+            System.out.println("T2: " + index);
+        }
+        System.out.println("T2 has stopped.");
+    }
 }
