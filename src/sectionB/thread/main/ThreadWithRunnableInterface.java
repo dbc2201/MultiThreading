8429931057daceb48eec995b9c3bbf4067c00186
dbc2201 <divyanshb22@gmail.com>
2020-10-23 12:16:24 +0530

    create ThreadWithRunnableInterface.java

---
+++ src/sectionB/thread/main/ThreadWithRunnableInterface.java
@@ -0,0 +1,11 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 23/10/20
+ *  Time: 12:14 PM
+ *  File Name : ThreadWithRunnableInterface.java
+ * */
+package sectionB.thread.main;
+
+public class ThreadWithRunnableInterface {
+
+}
