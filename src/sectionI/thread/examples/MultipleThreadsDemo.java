f1487682d8c0547d2ae1c7bc2793e972dd1345c4
dbc2201 <divyanshb22@gmail.com>
2020-10-23 16:04:56 +0530

    create MultipleThreadsDemo.java

---
+++ src/sectionI/thread/examples/MultipleThreadsDemo.java
@@ -0,0 +1,11 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 23/10/20
+ *  Time: 4:04 PM
+ *  File Name : MultipleThreadsDemo.java
+ * */
+package sectionI.thread.examples;
+
+public class MultipleThreadsDemo {
+
+}
