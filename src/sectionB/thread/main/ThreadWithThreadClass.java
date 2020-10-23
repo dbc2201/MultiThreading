3c949040050458394f4e80ee9af72ad97476e87e
dbc2201 <divyanshb22@gmail.com>
2020-10-23 12:37:48 +0530

    create ThreadWithThreadClass.java

---
+++ src/sectionB/thread/main/ThreadWithThreadClass.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 23/10/20
+ *  Time: 12:37 PM
+ *  File Name : ThreadWithThreadClass.java
+ * */
+package sectionB.thread.main;
+
+public class ThreadWithThreadClass {
+}
