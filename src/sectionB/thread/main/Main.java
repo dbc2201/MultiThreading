eb9e836e0f6b85ab73eded415a5ddb28eb0a211b
dbc2201 <divyanshb22@gmail.com>
2020-10-23 12:08:34 +0530

    create a reference variable to store the reference to the main thread

--- src/sectionB/thread/main/Main.java
+++ src/sectionB/thread/main/Main.java
@@ -8,6 +8,8 @@ package sectionB.thread.main;
 
 public class Main {
     public static void main(String[] args) {
-
+        // we have to store the reference to the main thread
+        // inside a reference variable of the Thread type.
+        Thread mainThread;
     }
 }
