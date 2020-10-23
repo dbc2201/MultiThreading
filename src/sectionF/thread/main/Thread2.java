841b19233502691f22dec306d3358dc5815ddc8e
dbc2201 <divyanshb22@gmail.com>
2020-10-23 13:35:26 +0530

    create Thread2.java

---
+++ src/sectionF/thread/main/Thread2.java
@@ -0,0 +1,11 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 23/10/20
+ *  Time: 1:35 PM
+ *  File Name : Thread2.java
+ * */
+package sectionF.thread.main;
+
+public class Thread2 {
+
+}
