b15e733255a68be70accecf9fddc8d7c7cc1ac00
dbc2201 <divyanshb22@gmail.com>
2020-10-23 12:04:05 +0530

    create Main.java

---
+++ src/sectionB/thread/main/Main.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 23/10/20
+ *  Time: 12:03 PM
+ *  File Name : Main.java
+ * */
+package sectionB.thread.main;
+
+public class Main {
+}
