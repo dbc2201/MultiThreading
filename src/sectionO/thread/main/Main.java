091232b438a38dc6591044a772b13df7ad216c5b
dbc2201 <divyanshb22@gmail.com>
2020-10-23 15:38:15 +0530

    create a reference variable of the Thread type

--- src/sectionO/thread/main/Main.java
+++ src/sectionO/thread/main/Main.java
@@ -8,6 +8,14 @@ package sectionO.thread.main;
 
 public class Main {
     public static void main(String[] args) {
-
+        // The compiler will automatically
+        // generate a thread to run this main() method.
+        // The thread will be aptly called "main".
+        // 1. All the other threads will be created from the main thread.
+        // 2. It is the last thread to stop execution because the compiler
+        // uses it for cleanup operations.
+        // To interact with the main thread, we have to create a reference
+        // variable of the Thread type.
+        Thread mainThread;
     }
 }
