1b1afc30c6d1b0b793ae5fb9a9ae10e28e1e7330
divyansh <divyanshb22@gmail.com>
2020-10-27 11:36:17 +0530

    sectionF: add a task for the CustomThread

--- src/sectionF/thread/main/CustomThread.java
+++ src/sectionF/thread/main/CustomThread.java
@@ -1,8 +1,15 @@
 package sectionF.thread.main;
 
+import javax.swing.JOptionPane;
+
 public class CustomThread implements Runnable {
     @Override
     public void run() {
-
+        JOptionPane.showMessageDialog(
+                null,
+                "The thread has started!",
+                "Custom Thread",
+                JOptionPane.WARNING_MESSAGE
+        );
     }
 }
