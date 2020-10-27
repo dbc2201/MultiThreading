d83b3a7e8aa9199aa7cea8bd48d61fd775ddc5e9
divyansh <divyanshb22@gmail.com>
2020-10-27 11:46:43 +0530

    sectionF: set the priority of the custom thread; eod

--- src/sectionF/thread/main/CustomThread.java
+++ src/sectionF/thread/main/CustomThread.java
@@ -10,6 +10,10 @@ public class CustomThread implements Runnable {
     public CustomThread(String threadName) {
         this.threadName = threadName;
         this.thread = new Thread(this, threadName);
+        this.thread.setPriority(Thread.MAX_PRIORITY);
+        // 1 -> Minimum Priority
+        // 5 -> Normal Priority (Default Priority)
+        // 10 -> Maximum Priority
     }
 
     public Thread getThread() {
@@ -24,7 +28,9 @@ public class CustomThread implements Runnable {
     public void run() {
         JOptionPane.showMessageDialog(
                 null,
-                threadName + " has started!",
+                threadName + " has started!" +
+                "\n Thread Priority: " + thread.getPriority() +
+                "\n Thread State: " + thread.getState() + ".",
                 "Custom Thread",
                 JOptionPane.WARNING_MESSAGE
         );
@@ -39,5 +45,12 @@ public class CustomThread implements Runnable {
                     JOptionPane.INFORMATION_MESSAGE
             );
         };
+    }
+}
+
+class CustomThreadExecution {
+    public static void main(String[] args) {
+        CustomThread thread = new CustomThread("PUBG");
+        thread.getThread().start();
     }
 }
