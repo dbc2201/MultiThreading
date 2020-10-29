dc359db02a1cbfe011d7c038c8b44d44dc3e2131
divyansh <divyanshb22@gmail.com>
2020-10-29 10:29:07 +0530

    sectionK: add a method to create and run a lambda thread

--- src/sectionK/thread/main/DemoLambdaThread.java
+++ src/sectionK/thread/main/DemoLambdaThread.java
@@ -1,5 +1,17 @@
 package sectionK.thread.main;
 
+import javax.swing.*;
+
 public class DemoLambdaThread {
+
+    public void createLambdaThread() {
+        Runnable lambdaThread = () -> JOptionPane.showMessageDialog(
+                null,
+                "This thread was created inside a method of a definition class!",
+                "Lambda Thread",
+                JOptionPane.WARNING_MESSAGE
+        );
+        lambdaThread.run();
+    }
 
 }
