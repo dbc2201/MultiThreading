32c6a56ef29ba753c31dbbe185e785c49bbf10c3
divyansh <divyanshb22@gmail.com>
2020-10-29 10:25:00 +0530

    sectionK: create a lambda thread and an anonymous thread

--- src/sectionK/thread/main/LambdaExpressionThread.java
+++ src/sectionK/thread/main/LambdaExpressionThread.java
@@ -1,5 +1,29 @@
 package sectionK.thread.main;
 
-public class LambdaExpressionThread {
+import javax.swing.*;
 
+public class LambdaExpressionThread {
+    public static void main(String[] args) {
+        // Create a reference variable of the Runnable Functional Interface type.
+        // Anonymous Class (Anonymous: with no name) -> A class with no name!
+        Runnable anonymousClassThread = new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "The Anonymous Class Thread is running!",
+                        "Anonymous Class Thread",
+                        JOptionPane.QUESTION_MESSAGE
+                );
+            }
+        } ;
+        anonymousClassThread.run();
+        Runnable lambdaThread = () -> JOptionPane.showConfirmDialog(
+                null,
+                "The Lambda Thread is running!",
+                "Lambda Thread",
+                JOptionPane.DEFAULT_OPTION
+        );
+        lambdaThread.run();
+    }
 }
