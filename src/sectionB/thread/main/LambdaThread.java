4d6b4114df4b21aa88048d5a11b59fb697251ac6
divyansh <divyanshb22@gmail.com>
2020-10-29 13:19:41 +0530

    sectionB: create a thread by using an anonymous class and a thread by using lambda expression

--- src/sectionB/thread/main/LambdaThread.java
+++ src/sectionB/thread/main/LambdaThread.java
@@ -1,5 +1,31 @@
 package sectionB.thread.main;
 
+import javax.swing.*;
+
 public class LambdaThread {
 
+}
+class LambdaThreadMain {
+    public static void main(String[] args) {
+        // Anonymous Class (Anonymous: with no name) -> Is a class with no name!
+        Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "The Anonymous Class Thread is running!",
+                        "Thread for Anonymous Class",
+                        JOptionPane.PLAIN_MESSAGE
+                );
+            }
+        };
+        thread.run();
+        Runnable lambdaThread = () -> JOptionPane.showConfirmDialog(
+                null,
+                "The Lambda Thread is running!",
+                "Lambda Thread",
+                JOptionPane.DEFAULT_OPTION
+        );
+        lambdaThread.run();
+    }
 }
