4dea9026aa36dc10d9974201749e5500d9304911
divyansh <divyanshb22@gmail.com>
2020-10-29 13:23:28 +0530

    sectionB: add a method to create and run a thread by using lambda expression

--- src/sectionB/thread/main/LambdaThread.java
+++ src/sectionB/thread/main/LambdaThread.java
@@ -3,10 +3,24 @@ package sectionB.thread.main;
 import javax.swing.*;
 
 public class LambdaThread {
-
+    public void createLambdaExpressionThread() {
+        Runnable lambdaExpressionThread = () -> JOptionPane.showOptionDialog(
+                null,
+                "The Lambda Thread from inside the method",
+                "Lambda Thread Inside The Method",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                null
+        );
+        lambdaExpressionThread.run();
+    }
 }
 class LambdaThreadMain {
     public static void main(String[] args) {
+        LambdaThread t1 = new LambdaThread();
+        t1.createLambdaExpressionThread();
         // Anonymous Class (Anonymous: with no name) -> Is a class with no name!
         Runnable thread = new Runnable() {
             @Override
